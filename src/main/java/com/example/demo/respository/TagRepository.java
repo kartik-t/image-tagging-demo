9f562e23981c4f8783f59d6baa073c65b48cec81
Kartik <kartiktiwari7@gmail.com>
2022-08-09 10:00:21 -0500

    Removed unused class

--- src/main/java/com/example/demo/respository/TagRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.example.demo.respository;
-//
-//import com.example.demo.entity.Tag;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface TagRepository extends CrudRepository<Tag, Integer> {
-//    Tag findById(Long tagId);
-//}
